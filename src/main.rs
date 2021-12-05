9c83a15a49f717cfa367c62ea2c19e7b778aee10
mbrizic <brizic.mario@gmail.com>
2021-12-05 17:40:43 +0100

    Add task 04 boilerplate

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,7 @@
 mod task_01;
 mod task_02;
 mod task_03;
+mod task_04;
 
 pub mod common;
 
@@ -9,8 +10,9 @@ fn main() {
     println!("| Task \t| Time \t\t| Answer");
     println!("|-------|---------------|---------------");
 
-    task_01::solution::run();
-    task_02::solution::run();
-    task_03::solution::run();
+    // task_01::solution::run();
+    // task_02::solution::run();
+    // task_03::solution::run();
+    task_04::solution::run();
 }
 
